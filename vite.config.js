5331292c045942d084ebf1b9997cb8b33519aeaf
Debadipta sahu <debssahu123@gmail.com>
2025-07-17 21:41:30 +0530

    Fix vite.svg 404 by setting base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-/** @type {import('vite').UserConfig} */
-export default {
+export default defineConfig({
   plugins: [react()],
-};
+  base: './', // 👈 This fixes the 404 error on Netlify
+});
